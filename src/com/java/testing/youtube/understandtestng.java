6601388d2788308966deab1675bae0a7be3d0b8f
mahbub095 <mahbub.095@gmail.com>
2020-02-21 03:01:13 +0600

    TestNg

---
+++ src/com/java/testing/youtube/understandtestng.java
@@ -0,0 +1,14 @@
+package com.java.testing.youtube;
+
+import org.testng.annotations.Test;
+
+public class understandtestng {
+
+ 
+	
+	@Test
+	public void testng() {
+		System.out.println("Understanding testng.xml File");
+	}
+	
+}
